9c3e1a7126ed543b1032c3d767a5730d4ccc1533
astrapi69 <asterios.raptis@gmx.net>
2020-01-11 15:36:47 +0100

    update from build.gradle
    - added license plugin
    - added jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
-import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.gradle.jvm.tasks.Jar
 import java.net.URI
 
-val servletApiVersion = "3.1.0"
+val servletApiVersion = "4.0.1"
 val commonsIoVersion = "2.6"
 
 plugins {
     signing
+    jacoco
     kotlin("jvm") version "1.3.61"
     id("java")
     id("maven-publish")
-    id("org.jetbrains.dokka") version "0.9.17"
+    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.hierynomus.license") version "0.15.0"
+    id("org.jetbrains.dokka") version "0.10.0"
 }
 
 group = "de.alpharogroup"
-version = "1.1"
+version = "1.2-SNAPSHOT"
 description = "servlet-kt-extensions"
 
 
@@ -22,6 +24,12 @@ repositories {
     jcenter()
     mavenLocal()
     mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("javax.servlet:javax.servlet-api:$servletApiVersion")
+    implementation("commons-io:commons-io:$commonsIoVersion")
 }
 
 // Configure existing Dokka task to output HTML to typical Javadoc directory
@@ -39,13 +47,6 @@ val dokkaJar by tasks.creating(Jar::class) {
     from(tasks.dokka)
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("javax.servlet:javax.servlet-api:$servletApiVersion")
-    implementation("commons-io:commons-io:$commonsIoVersion")
-
-}
-
 val sourcesJar by tasks.registering(Jar::class) {
     archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
@@ -60,7 +61,7 @@ publishing {
 
             pom {
                 name.set(rootProject.name)
-                url.set("https://github.com/astrapi69/"+ rootProject.name)
+                url.set("https://github.com/astrapi69/" + rootProject.name)
                 description.set("Extension methods for servlets, requests and response objects")
                 organization {
                     name.set("Alpha Ro Group UG (haftungsbeschrängt)")
@@ -68,7 +69,7 @@ publishing {
                 }
                 issueManagement {
                     system.set("GitHub")
-                    url.set("https://github.com/astrapi69/"+ rootProject.name +"/issues")
+                    url.set("https://github.com/astrapi69/" + rootProject.name + "/issues")
                 }
                 licenses {
                     license {
@@ -84,9 +85,9 @@ publishing {
                     }
                 }
                 scm {
-                    connection.set("scm:git:git:@github.com:astrapi69/"+ rootProject.name +".git")
-                    developerConnection.set("scm:git:git@github.com:astrapi69/"+ rootProject.name +".git")
-                    url.set("git:@github.com:astrapi69/"+ rootProject.name +".git")
+                    connection.set("scm:git:git:@github.com:astrapi69/" + rootProject.name + ".git")
+                    developerConnection.set("scm:git:git@github.com:astrapi69/" + rootProject.name + ".git")
+                    url.set("git:@github.com:astrapi69/" + rootProject.name + ".git")
                 }
             }
 
@@ -103,13 +104,55 @@ publishing {
                     val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
                     val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
                     val projectVersion = version.toString()
-                    val urlString = if(projectVersion.endsWith("SNAPSHOT"))  snapshotsRepoUrl else releasesRepoUrl
+                    val urlString = if (projectVersion.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
                     url = URI.create(urlString)
                 }
             }
         }
     }
 }
+
+license {
+    ext.set("year", "2015")
+    ext.set("owner", "Asterios Raptis")
+    header = rootProject.file("src/main/resources/LICENSE.txt")
+    val value = listOf(
+        "**/README",
+        "**/README.md",
+        "**/LICENSE",
+        "**/NOTICE",
+        "**/*.xml",
+        "**/*.xsl",
+        "**/*.xsd",
+        "**/*.dtd",
+        "**/*.html",
+        "**/*.json",
+        "**/*.jsp",
+        "**/*.jpa",
+        "**/*.sql",
+        "**/*.properties",
+        "**/*.bat",
+        "**/*.gradle",
+        "**/*.MF",
+        "**/*.txt",
+        "**/*.vm",
+        "**/*.log",
+        "**/*.map",
+        "**/*.js.map",
+        "**/*.tmpl",
+        "**/*.js.tmpl",
+        "**/*.editorconfig",
+        "src/test/resources/**",
+        "src/main/resources/**",
+        "out/**",
+        "build/**"
+    )
+    excludes(value)
+}
+
+// workhack for license issue #76
+gradle.startParameter.excludedTaskNames += "licenseMain"
+gradle.startParameter.excludedTaskNames += "licenseTest"
 
 signing {
     sign(publishing.publications["mavenJava"])
@@ -122,4 +165,43 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+tasks.test {
+    useTestNG()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.isEnabled = true
+        html.destination = file("$buildDir/reports/coverage")
+    }
+    classDirectories.setFrom(
+        sourceSets.main.get().output.asFileTree.matching {
+            // exclude main()
+            exclude("")
+        }
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.0".toBigDecimal()
+            }
+        }
+    }
+}
+
+val testCoverage by tasks.registering {
+    group = "verification"
+    description = "Runs the unit tests with coverage."
+
+    dependsOn(":test", ":jacocoTestReport", ":jacocoTestCoverageVerification")
+    val jacocoTestReport = tasks.findByName("jacocoTestReport")
+    jacocoTestReport?.mustRunAfter(tasks.findByName("test"))
+    tasks.findByName("jacocoTestCoverageVerification")?.mustRunAfter(jacocoTestReport)
 }
