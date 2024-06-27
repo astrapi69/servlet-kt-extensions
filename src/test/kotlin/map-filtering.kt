f61a87d76bf88dc08aa7afcb4ba0584e03141f87
Asterios Raptis <asteri.raptis@gmail.com>
2024-06-27 16:42:02 +0200

    new dependency mockito-core in version 5.12.0
    - new test class HttpServletRequestExtensionsTest created
    - new dependency mockito-junit-jupiter in version 5.12.0
    - update of gradle-plugin dependency with id 'com.diffplug.spotless' to new beta version 7.0.0.BETA1
    - update of gradle-plugin dependency with id 'com.github.ben-manes.versions' to new minor version 0.51.0
    - update of gradle-plugin dependency with id 'org.jetbrains.kotlin.jvm' to new beta version 2.0.20-Beta1
    - update of dependency commons-io to new version 2.16.1
    - update of dependency jakarta-servlet-api to new version 6.1.0
    - update of test dependency junit-jupiter to new version 5.11.0-M2

--- src/test/kotlin/map-filtering.kt
+++ src/test/kotlin/map-filtering.kt
@@ -18,7 +18,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-fun main(args: Array<String>) {
+fun main() {
 
   val originalMap = mapOf(1 to 0, 2 to 1, 3 to 3)
   val filteredMap = originalMap.filterValues { it > 0 }
